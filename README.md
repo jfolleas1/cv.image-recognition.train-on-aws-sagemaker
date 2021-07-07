fa8d83bd6bb5eaf4e8e18eff7ce2f87287750069
Jacques Folleas <jacquesfolleas@Jacquess-MacBook-Pro.local>
2021-07-07 21:45:40 +0100

    first working commit

---
+++ README.md
@@ -0,0 +1,56 @@
+## Upload the dataset on S3
+
+`aws s3 mb s3://YOUR-DATA-BUCKET-NAME`
+
+aws s3 mb s3://jac.test-sagemaker.data
+
+`aws s3 mb s3://YOUR-OUTPUT-BUCKET-NAME`
+
+aws s3 mb s3://jac.test-sagemaker.output
+
+`aws s3 sync DATA_FOLDER s3://YOUR-DATA-BUCKET-NAME`
+
+aws s3 sync flowers s3://jac.test-sagemaker.data
+
+
+```
+account=$(aws sts get-caller-identity --query Account --output text)
+region=$(aws configure get region)
+aws ecr get-login-password \
+    --region ${region} \
+| docker login \
+    --username AWS \
+    --password-stdin ${account}.dkr.ecr.${region}.amazonaws.com
+```
+
+```
+algorithm_name=sagemaker-tf-flower-example
+fullname="${account}.dkr.ecr.${region}.amazonaws.com/${algorithm_name}:latest"
+docker build  -t ${algorithm_name} container
+```
+
+```
+aws ecr create-repository --repository-name ${algorithm_name}
+docker tag ${algorithm_name} ${fullname}
+docker push ${fullname}
+```
+
+
+`aws iam create-role --role-name SagemakerRole --assume-role-policy-document file://./role-policy.json`
+
+`aws iam attach-role-policy --role-name SagemakerRole --policy-arn arn:aws:iam::aws:policy/AmazonSageMakerFullAccess`
+
+`aws iam put-role-policy --role-name SagemakerRole --policy-document file://./s3-policy.json --policy-name s3-policy`
+
+```
+aws sagemaker create-training-job --cli-input-json file://training-job-config.json
+```
+
+
+algorithm_name=sagemaker-tf-flower-example
+fullname="${account}.dkr.ecr.${region}.amazonaws.com/${algorithm_name}:latest"
+docker build  -t ${algorithm_name} container
+aws ecr create-repository --repository-name ${algorithm_name}
+docker tag ${algorithm_name} ${fullname}
+docker push ${fullname}
+aws sagemaker create-training-job --cli-input-json file://training-job-config.json
---
+++ container/modeling/train.py
@@ -0,0 +1,132 @@
+import os
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+from config import INPUT_IMG_SIZE, BATCH_SIZE
+
+DATA_DIR = "/opt/ml/input/data/flowers"
+# DATA_DIR = "./flowers"
+OUTPUT_DIR = "/opt/ml/model/"
+
+
+def build_data_pipelines():
+  train_data_path = os.path.join(DATA_DIR, 'training')
+  val_data_path = os.path.join(DATA_DIR, 'validation')
+  eval_data_path = os.path.join(DATA_DIR, 'evaluation')
+
+  train_augmentor = ImageDataGenerator(
+    rescale = 1. / 255,
+    rotation_range=25,
+    zoom_range=0.15,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.15,
+    horizontal_flip=True,
+    fill_mode="nearest"
+  )
+
+  val_augmentor = ImageDataGenerator(
+    rescale = 1. / 255
+  )
+
+  eval_augmentor = ImageDataGenerator(
+    rescale = 1. / 255
+  )
+
+  train_generator = train_augmentor.flow_from_directory(
+    train_data_path,
+    class_mode="categorical",
+    target_size=INPUT_IMG_SIZE,
+    color_mode="rgb",
+    shuffle=True,
+    batch_size=BATCH_SIZE
+  )
+
+  val_generator = val_augmentor.flow_from_directory(
+    val_data_path,
+    class_mode="categorical",
+    target_size=INPUT_IMG_SIZE,
+    color_mode="rgb",
+    shuffle=False,
+    batch_size=BATCH_SIZE
+  )
+
+  eval_generator = eval_augmentor.flow_from_directory(
+    eval_data_path,
+    class_mode="categorical",
+    target_size=INPUT_IMG_SIZE,
+    color_mode="rgb",
+    shuffle=False,
+    batch_size=BATCH_SIZE
+  )
+
+  return train_generator, val_generator, eval_generator
+
+
+def train():
+
+  # image_gen = ImageDataGenerator(
+  #   rescale=1./255,
+  #   validation_split=0.2
+  # )
+
+  # train_data_gen = image_gen.flow_from_directory(
+  #   batch_size=BATCH_SIZE,
+  #   directory=os.path.join(DATA_DIR, "training"),
+  #   subset="training",
+  #   shuffle=True,
+  #   target_size=IMG_SIZE,
+  #   class_mode='sparse'
+  # )
+
+  # test_data_gen = image_gen.flow_from_directory(
+  #   batch_size=BATCH_SIZE,
+  #   directory=os.path.join(DATA_DIR, "training"),
+  #   subset="validation",
+  #   target_size=IMG_SIZE,
+  #   class_mode='sparse'
+  # )
+
+  train_generator, val_generator, eval_generator = build_data_pipelines()
+
+  classes_dict = train_data_gen.class_indices
+
+  print("classes_dict : \n ", classes_dict)
+
+
+  model = tf.keras.models.Sequential([
+      tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(*IMG_SIZE, 3)),
+      tf.keras.layers.MaxPooling2D(2, 2),
+      tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+      tf.keras.layers.MaxPooling2D(2,2),
+      tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+      tf.keras.layers.MaxPooling2D(2,2),
+      tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+      tf.keras.layers.MaxPooling2D(2,2),
+      tf.keras.layers.Dropout(0.5),
+      tf.keras.layers.Flatten(),
+      tf.keras.layers.Dense(512, activation='relu'),
+      tf.keras.layers.Dense(len(classes_dict), activation='softmax')
+  ])
+
+  model.compile(
+    optimizer=tf.keras.optimizers.Adam(lr=1e-4),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+    metrics=["accuracy"]
+  )
+ 
+  print(f" $ ls {DATA_DIR} : ")
+  print(os.listdir(DATA_DIR))
+  print(f" $ ls {os.path.join(DATA_DIR, 'training')} : ")
+  print(os.listdir(os.path.join(DATA_DIR, "training")))
+
+
+  model.fit(train_data_gen, epochs=2, steps_per_epoch=20)
+  test_loss, test_accuracy = model.evaluate(test_data_gen, steps=100)
+  print(f"Test accuracy: {test_accuracy}")
+
+  model.save(OUTPUT_DIR + "model.h5")
+
+
+if __name__=="__main__":
+  train()
