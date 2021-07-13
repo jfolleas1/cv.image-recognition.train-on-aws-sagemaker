110e2d9243bd1f46e1b48605c0731277cc837c10
Jacques Folleas <jacquesfolleas@Jacquess-MacBook-Pro.local>
2021-07-13 09:50:03 +0100

    Change sagemaker s3 buckets names

--- project_manager.py
+++ project_manager.py
@@ -22,9 +22,9 @@ def init_s3_buckets():
     """
     Create the necessary AWS S3 bucket and the policies with it.
     """
-    os.system(f"aws s3 mb s3://{cfg.PROJECT_NAME}.data")
-    os.system(f"aws s3 mb s3://{cfg.PROJECT_NAME}.output")
-    os.system(f"aws s3 sync {cfg.DATA_LOCAL_DIR} s3://{cfg.PROJECT_NAME}.data")
+    os.system(f"aws s3 mb s3://{cfg.PROJECT_NAME}.smtj.data")
+    os.system(f"aws s3 mb s3://{cfg.PROJECT_NAME}.smtj.output")
+    os.system(f"aws s3 sync {cfg.DATA_LOCAL_DIR} s3://{cfg.PROJECT_NAME}.smtj.data")
     os.system(f"aws iam create-role --role-name SagemakerRole " +\
         "--assume-role-policy-document file://./role-policy.json")
     os.system(f"aws iam attach-role-policy --role-name SagemakerRole " +\
